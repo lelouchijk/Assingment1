69e4aba976da000471d9f328f1f1cdc356a51aad
Pyae Phyo Aung <pyaephyoaungmonywa@gmail.com>
2024-10-21 11:10:18 +0630

    bootstrap to css

--- sell/src/main/java/com/sell/repository/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sell.repository;
-
-import com.sell.model.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company,Long> {
-}
